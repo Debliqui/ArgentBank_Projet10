5ad35ddb8bdf4d2d684afcc776951cb0ac5b2ca7
Debliqui Valentin <debliqui.valentin@outlook.com>
2025-01-31 09:59:00 -0500

    Adding a Banner component

--- argentBank-Frontend/src/pages/Home/index.jsx
+++ argentBank-Frontend/src/pages/Home/index.jsx
@@ -1,13 +1,21 @@
 import Footer from "../../common/components/Footer"
 import Header from "../../common/components/Header"
+import Banner from "../../common/components/Banner"
+import "./index.scss"
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div>
-        <h2>Home</h2>
-      </div>
+      <main>
+        <Banner>
+          <h2 className="sr-only">Promoted Content</h2>
+          <p className="subtitle">No fees.</p>
+          <p className="subtitle">No minimum deposit.</p>
+          <p className="subtitle">High interest rates.</p>
+          <p className="text">Open a savings account with Argent Bank today!</p>
+        </Banner>
+      </main>
       <Footer />
     </>
   )
