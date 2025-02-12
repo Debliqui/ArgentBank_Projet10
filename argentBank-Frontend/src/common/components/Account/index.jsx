a4cb96aba4172b95e9a37b37b15c174f146d74f0
Debliqui Valentin <debliqui.valentin@outlook.com>
2025-02-12 10:25:54 -0500

    Add a props for amount currency

--- argentBank-Frontend/src/common/components/Account/index.jsx
+++ argentBank-Frontend/src/common/components/Account/index.jsx
@@ -2,15 +2,21 @@ import PropTypes from "prop-types"
 import "./index.scss"
 
 export default function Account({
+  locale = "en-US",
+  currency = "USD",
   title = "Argent Bank",
-  amount = "1000",
+  amount = "1000.00",
   amountDescription = "Available Balance",
 }) {
+  const formattedAmount = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(amount)
   return (
     <section className="account">
       <div className="account__content-wrapper">
         <h4 className="account__title">{title}</h4>
-        <p className="account__amount">{`$ ${amount}`}</p>
+        <p className="account__amount">{formattedAmount}</p>
         <p className="account__amount__description">{amountDescription}</p>
       </div>
       <div className="account__content-wrapper cta">
@@ -21,7 +27,9 @@ export default function Account({
 }
 
 Account.propTypes = {
+  locale: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  amount: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
   amountDescription: PropTypes.string,
 }
